55e289bb5397c9c13585cfe22cd0b6d865fe4267
deshmukhakash55 <deshmukhakash55@gmail.com>
2019-03-06 00:12:54 +0530

    removed new line

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,5 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("I am Akash");
-        System.out.println("Added new line from git console itself");
     }
 }
