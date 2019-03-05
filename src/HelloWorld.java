31d75c9fc742fdf2fed06f01665db4ba121902ee
deshmukhakash55 <deshmukhakash55@gmail.com>
2019-03-06 00:54:19 +0530

    removed new line

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.print("I am akash");
         System.out.println("Keep it as it is");
     }
 }
